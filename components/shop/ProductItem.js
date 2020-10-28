b0f989335a253074672e15461bdc851ee0609a18
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-28 13:57:40 -0400

    added touchable component so entire card can be pressed

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,27 +1,48 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 import Colors from "../../constants/Colors";
 const ProductItem = props => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
   return (
     <View style={styles.product}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: props.image }} />
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.title}>{props.title}</Text>
-        <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.actions}>
-        <Button
-          color={Colors.primary}
-          title='View details'
-          onPress={props.onViewDetail}
-        />
-        <Button
-          color={Colors.primary}
-          title='Go to cart'
-          onPress={props.onAddToCart}
-        />
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title='View details'
+                onPress={props.onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title='Go to cart'
+                onPress={props.onAddToCart}
+              />
+            </View>
+          </View>
+        </TouchableCmp>
       </View>
     </View>
   );
@@ -63,6 +84,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: "15%",
     padding: 10,
+  },
+  touchable: {
+    overflow: "hidden",
+    borderRadius: 10,
   },
 });
 export default ProductItem;
