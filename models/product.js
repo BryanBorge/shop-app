a3915fbb27ddb24e982b2d6d2987e50a906c6685
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-28 12:30:51 -0400

    added product data model

---
+++ models/product.js
@@ -0,0 +1,12 @@
+class Product {
+  constructor(id, ownerId, title, imageURL, desc, price) {
+    this.id = id;
+    this.ownerId = ownerId;
+    this.title = title;
+    this.imageURL = imageURL;
+    this.desc = desc;
+    this.price = price;
+  }
+}
+
+export default Product;
