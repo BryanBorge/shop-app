66857c028dceece9803951157ccadd0f83667e49
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-28 16:56:15 -0400

    Added Header button for cart

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { FlatList, Text, View, StyleSheet } from "react-native";
-import { useSelector } from "react-redux";
+import { FlatList, Text, View, StyleSheet, Platform } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
+import HeaderButton from "../../components/UI/HeaderButton";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import * as cartActions from "../../store/actions/cart";
+
 const ProductsOverviewScreen = props => {
+  const dispatch = useDispatch();
+
   const products = useSelector(state => state.products.availableProducts);
   return (
     <FlatList
@@ -18,15 +24,28 @@ const ProductsOverviewScreen = props => {
               productTitle: itemData.item.title,
             });
           }}
-          onAddToCart={() => {}}
+          onAddToCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
         />
       )}
     />
   );
 };
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: "All Products",
+ProductsOverviewScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "All Products",
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Cart'
+          iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          onPress={() => navData.navigation.navigate("Cart")}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({});
