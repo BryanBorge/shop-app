195e67db329b4af50ef001142bb7058e99ae5827
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-29 14:57:59 -0400

    Now uses Card component

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,7 +5,7 @@ import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
 import * as orderActions from "../../store/actions/order";
 import * as cartActions from "../../store/actions/cart";
-
+import Card from "../../components/UI/Card";
 const CartScreen = props => {
   const cartTotalAmount = useSelector(state => state.cart.totalAmount);
   const items = useSelector(state => state.cart.items);
@@ -28,20 +28,23 @@ const CartScreen = props => {
   const dispatch = useDispatch();
   return (
     <View style={styles.screen}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:{" "}
-          <Text style={styles.amount}> ${cartTotalAmount.toFixed(2)}</Text>
+          <Text style={styles.amount}>
+            {" "}
+            ${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}
+          </Text>
         </Text>
         <Button
-          title='Order now'
+          title='ORDER NOW'
           color={Colors.accent}
           disabled={cartItems.length === 0}
           onPress={() => {
             dispatch(orderActions.addOrder(cartItems, cartTotalAmount));
           }}
         />
-      </View>
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={item => item.productId}
@@ -73,13 +76,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginBottom: 20,
     padding: 10,
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
   },
   summaryText: { fontFamily: "open-sans-bold", fontSize: 18 },
   amount: { color: Colors.primary },
