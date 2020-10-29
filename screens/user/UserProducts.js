e58eb9b522fcd425adffe22d6fc23f808ee5dcac
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-29 14:59:01 -0400

    Added delete handler and added right header button

--- screens/user/UserProducts.js
+++ screens/user/UserProducts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, Button, StyleSheet, Platform } from "react-native";
+import { FlatList, Button, StyleSheet, Platform, Alert } from "react-native";
 import ProductItem from "../../components/shop/ProductItem";
 import { useSelector, useDispatch } from "react-redux";
 import HeaderButton from "../../components/UI/HeaderButton";
@@ -9,6 +9,24 @@ import * as productActions from "../../store/actions/products";
 const UserProductsScreen = props => {
   const userProducts = useSelector(state => state.products.userProducts);
   const dispatch = useDispatch();
+
+  const editProductHandler = id => {
+    props.navigation.navigate("EditProduct", { productId: id });
+  };
+
+  const deleteHandler = id => {
+    Alert.alert("Are You sure", "Do you really want to delte this item?", [
+      { text: "No", style: "default" },
+      {
+        text: "Yes",
+        style: "destructive",
+        onPress: () => {
+          dispatch(productActions.deleteProduct(id));
+        },
+      },
+    ]);
+  };
+
   return (
     <FlatList
       data={userProducts}
@@ -18,15 +36,21 @@ const UserProductsScreen = props => {
           image={itemData.item.imageURL}
           title={itemData.item.title}
           price={itemData.item.price}
-          onSelect={() => {}}
+          onSelect={() => {
+            editProductHandler(itemData.item.id);
+          }}
         >
-          <Button color={Colors.primary} title='Edit' onPress={() => {}} />
+          <Button
+            color={Colors.primary}
+            title='Edit'
+            onPress={() => {
+              editProductHandler(itemData.item.id);
+            }}
+          />
           <Button
             color={Colors.primary}
             title='Delete'
-            onPress={() => {
-              dispatch(productActions.deleteProduct(itemData.item.id));
-            }}
+            onPress={deleteHandler.bind(this, itemData.item.id)}
           />
         </ProductItem>
       )}
@@ -43,6 +67,15 @@ UserProductsScreen.navigationOptions = navData => {
           title='Menu'
           iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
           onPress={() => navData.navigation.toggleDrawer()}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Add'
+          iconName={Platform.OS === "android" ? "md-create" : "ios-create"}
+          onPress={() => navData.navigation.navigate("EditProduct")}
         />
       </HeaderButtons>
     ),
