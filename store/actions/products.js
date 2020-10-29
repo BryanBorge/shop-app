f2af8b990567bdea83a35a069345d434c58d84a3
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-29 14:59:17 -0400

    created actions/action creators for update and create product

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,5 +1,22 @@
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
+};
+
+export const createProduct = (title, imageURL, price, desc) => {
+  return {
+    type: CREATE_PRODUCT,
+    productData: { title, imageURL, price, desc },
+  };
+};
+
+export const updateProduct = (id, title, imageURL, desc) => {
+  return {
+    type: UPDATE_PRODUCT,
+    pid: id,
+    productData: { title, imageURL, desc },
+  };
 };
