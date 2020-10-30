2e73faa2b1638043643ad051a83e3fa9f196b45d
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-10-30 11:42:44 -0400

    Added composeWithDevTools for debugging

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
 import orderReducer from "./store/reducers/order";
 import ShopNavigator from "./navigation/ShopNavigator";
 import { AppLoading } from "expo";
+import ReduxThunk from "redux-thunk";
 import * as Font from "expo-font";
 import { composeWithDevTools } from "redux-devtools-extension";
+
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
   orders: orderReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk))
+);
 
 const fetchFonts = async () => {
   return await Font.loadAsync({
